080693c8ee4d683e10fb863a969fc373b105ddba
Hartmut Holzgraefe <hartmut@php.net>
2021-09-18 12:27:22 +0200

    add support for various favicon formats

--- www/urls.py
+++ www/urls.py
@@ -27,6 +27,8 @@ import django
 from django.conf.urls import url, include
 from django.views.static import serve
 from django.views.generic import TemplateView
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -126,6 +128,10 @@ urlpatterns = [
     # robots.txt
     url(r'^robots\.txt$', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
 
+    # favicons 
+    url(r'^favicon\.png$', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.png'))),
+    url(r'^favicon\.ico$', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico'))),
+    url(r'^apple-touch-icon.*\.png$', RedirectView.as_view(url=staticfiles_storage.url('img/apple-touch-icon.png'))),
 ]
 
 if settings.DEBUG:

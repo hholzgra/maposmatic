74d0cf727129b87e5eeca44354b7f70ad6df539a
Hartmut Holzgraefe <hartmut@php.net>
2023-11-12 13:09:28 +0100

    merge

--- scripts/render.py
+++ scripts/render.py
@@ -404,7 +404,8 @@ class JobRenderer(threading.Thread):
                 img = Image.open(prefix + '.png')
                 try:
                     img = img.convert('RGB')
-                    img.save(prefix + '.jpg', quality=50)
+                    img.save(prefix + '.tmp.jpg', quality=50)
+                    os.rename(prefix + '.tmp.jpg', prefix + '.jpg')
                 except Exception as e:
                     LOG.warning("PNG to JPEG conversion failed: %s" % e)
                 img.thumbnail((200, 200), Image.ANTIALIAS)
@@ -414,9 +415,10 @@ class JobRenderer(threading.Thread):
             img.close()
 
             try:
-                pngquant_cmd = [ "pngquant", "--output", "%s.8bit.png" % prefix,
+                pngquant_cmd = [ "pngquant", "--output", "%s.tmp.8bit.png" % prefix,
                                  "%s.png" % prefix ]
                 subprocess.check_call(pngquant_cmd)
+                os.rename(prefix + '.tmp.8bit.png', prefix + '.8bit.png')
             except Exception as e:
                 LOG.warning("PNG color reduction failed: %s" % e)
 

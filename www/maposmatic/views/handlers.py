4539088e83897a7c66b1831db8fbec5f00e1457f
Hartmut Holzgraefe <hartmut@php.net>
2024-09-08 11:12:26 +0000

    remove now redundant exception mailing - handled by django itself now

--- www/maposmatic/views/handlers.py
+++ www/maposmatic/views/handlers.py
@@ -23,7 +23,6 @@ LOG = logging.getLogger('maposmatic')
 
 from django.shortcuts import render
 from django.template.loader import render_to_string, get_template
-from django.core import mail
 
 import www.settings
 from www.maposmatic import helpers, forms, models
@@ -32,24 +31,6 @@ def myhandler500(request):
     type, value, tb = sys.exc_info()
 
     tb_msg = "".join(traceback.format_exception(type, value, tb))
-
-    template = get_template("frontend_email_exception.txt")
-
-    context = { 'from':    www.settings.DAEMON_ERRORS_EMAIL_FROM,
-                'replyto': www.settings.DAEMON_ERRORS_EMAIL_REPLY_TO,
-                'to':      ', '.join(['%s <%s>' % admin for admin in www.settings.ADMINS]),
-                'type':    type,
-                'value':   value,
-                'tb':      tb_msg,
-                'url':     request.build_absolute_uri(),
-                'method':  request.method,
-                'date':    datetime.datetime.now().strftime('%a, %d %b %Y %H:%M:%S +0200 (CEST)'),
-               }
-
-    body = template.render(context)
-    subject = 'Web request exception caught'
-
-    mail.mail_admins(subject, body)
 
     return render(request,
                   '500.html',
